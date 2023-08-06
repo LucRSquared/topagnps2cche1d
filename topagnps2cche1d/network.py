2cb79c987c1ce1ce8faf3e9adcecf91287be9027
Luc Rébillout <LucRSquared@users.noreply.github.com>
2023-08-06 18:38:15 -0500

    method to assign strahler number

--- topagnps2cche1d/network.py
+++ topagnps2cche1d/network.py
@@ -1,4 +1,5 @@
 from tools import dfs_iterative_postorder
+import pandas as pd
 
 class Node:
     def __init__(self, id, type=None, usid=None, us2id=None, dsid=None, computeid=None, x=None, y=None):
@@ -12,17 +13,17 @@ class Node:
         self.y = y
 
 class Reach:
-    def __init__(self, id, ignore=False, strahler_order=None, us_nd_id=None, ds_nd_id=None, receiving_reach_id=None):
+    def __init__(self, id, ignore=False, us_nd_id=None, ds_nd_id=None, receiving_reach_id=None):
         self.id = id
         self.ignore = ignore
-        self.strahler_order = strahler_order
+        self.strahler_number = None
         self.us_nd_id = us_nd_id
         self.ds_nd_id = ds_nd_id
         self.receiving_reach_id = receiving_reach_id
-        self.nodes = []
+        self.nodes = {}
 
     def add_node(self, node):
-        self.nodes.append(node)
+        self.nodes[node.id] = node
 
     def ignore_reach(self):
         self.ignore = True
@@ -30,24 +31,29 @@ class Reach:
     def include_reach(self):
         self.ignore = False
 
+class Cell:
+    def __init__(self, id, receiving_reach_id):
+        self.id = id
+        self.cell_type = None # Left, Right, Source
+        self.receiving_reach_id = receiving_reach_id
+
 class System:
     def __init__(self):
-        self.reaches = []
+        self.reaches = {}
         self.connectivity_dict = {}
 
     def add_reach(self, reach):
-        self.reaches.append(reach)
+        self.reaches[reach.id] = reach
 
     def assess_connectivity(self):
         
         connectivity_dict = {}
 
-        for reach in self.reaches:
+        for reach_id, reach in self.reaches.items():
             
             if reach.ignore:
                 continue
 
-            reach_id = reach.id
             receiving_reach_id = reach.receiving_reach_id
 
             # Initialize the list for each channel if not already present
@@ -61,17 +67,50 @@ class System:
     def find_root_reach(self):
         connectivity_dict = self.connectivity_dict
 
-        for reach_test_root in self.reaches:
-            for upstream_reaches in connectivity_dict.values():
-                if reach_test_root.id not in upstream_reaches:
-                    return reach_test_root.id
+        all_upstream_reaches = []
+
+        for upstream_reaches in connectivity_dict.values():
+            all_upstream_reaches.extend(upstream_reaches)
+
+        for reach_id in self.reaches.keys():
+            if reach_id not in all_upstream_reaches:
+                return reach_id
 
 
     def assign_strahler_number_to_reaches(self):
         # def compute_
 
-        connectivity_dict = self.connectivity_dict
+        reaches = self.reaches()
 
-        for reach in self.reaches:
-            if reach.id not in connectivity_dict:
-                reach.strahler_number = 1
+        connectivity_dict = self.connectivity_dict
+        
+        root_id = self.find_root_reach()
+
+        # optimizing the order of reaches processed by using DFS algorithm
+        queue = dfs_iterative_postorder(connectivity_dict, root_id)
+
+        # Process the queue
+        while queue:
+            current_id = queue.pop(0) # take the first element
+            current_reach = reaches[current_id]
+
+            if current_id not in connectivity_dict:
+                # reach is at the most upstream end
+                current_reach.strahler_number = 1
+                continue
+
+            # get list of upstream reaches
+            upstream_reaches_strahler = [reaches[id].strahler_number for id in connectivity_dict[current_id]]
+
+            if None in upstream_reaches_strahler:
+                # undetermined case, keep current_reach in queue
+                queue.append(current_id)
+                continue
+            else:
+                max_strahler = max(upstream_reaches_strahler)
+                count_max_strahler = upstream_reaches_strahler.count(max_strahler)
+
+                if count_max_strahler >= 2:
+                    current_reach.strahler_number = max_strahler + 1
+                else:
+                    current_reach.strahler_number = max_strahler
