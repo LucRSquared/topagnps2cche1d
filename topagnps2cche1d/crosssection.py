30d38fe44f2c34a0a381920b1ddb68b37f72b088
Luc Rébillout <LucRSquared@users.noreply.github.com>
2023-08-23 11:29:05 -0500

    Working on CrossSection class

--- topagnps2cche1d/crosssection.py
+++ topagnps2cche1d/crosssection.py
@@ -1,3 +1,186 @@
+import numpy as np
+
+
 class CrossSection:
-    def __init__(self, id, **kwargs):
+    def __init__(
+        self,
+        id,
+        ws=[],
+        zs=[],
+        lfp_idx=None,
+        rfp_idx=None,
+        lbt_idx=None,
+        rbt_idx=None,
+        mannings_roughness=0.03,
+        **kwargs,
+    ):
         self.id = id
+        self.ws = np.array(ws)  # Transversal coordinates from left to right
+        self.zs = np.array(zs)  # Elevation values
+        self.lfp_idx = lfp_idx  # 'Left Flood Plain' edge Index of cross section marking the beginning of the main channel
+        self.rfp_idx = rfp_idx  # 'Left Bank Toe' Index of cross section marking the end of the main channel
+        self.lbt_idx = lbt_idx  # 'Left Bank Toe' index marking the toe of the left bank
+        self.rbt_idx = (
+            rbt_idx  # 'Right Bank Toe' index marking the toe of the right bank
+        )
+        self.n_rgh = mannings_roughness  # Manning's Roughness coefficient
+        # Can be an array of length len(ws) - 1 where n_rgh[i] is the roughness between ws[i] and ws[i+1]
+
+        if "type" in kwargs:
+            xs_type = kwargs["type"]
+        else:
+            xs_type = "default"
+
+        if xs_type == "default":
+            (
+                self.ws,
+                self.zs,
+                self.lfp_idx,
+                self.lbt_idx,
+                self.rbt_idx,
+                self.rfp_idx,
+            ) = self.define_trapz_cross_section_width_slope_elevation()
+        elif xs_type == "trapezoidal_with_flood_plain":
+            (
+                self.ws,
+                self.zs,
+                self.lfp_idx,
+                self.lbt_idx,
+                self.rbt_idx,
+                self.rfp_idx,
+            ) = self.define_trapz_cross_section_width_slope_elevation()
+
+        self._assert_valid_cross_section()
+
+    def _assert_valid_cross_section(self):
+        """
+        Make sure the provided cross-section is valid
+        """
+        ws, zs = self.ws, self.zs
+        lfp, lbt, rbt, rfp = self.lfp_idx, self.lbt_idx, self.rbt_idx, self.rfp_idx
+        if not (ws) or not (zs):
+            raise AssertionError("Empty cross section coordinates")
+        elif len(ws) != len(zs):
+            raise AssertionError("Ws and Zs coordinates should be the same length")
+        elif any([item is None for item in [lfp, rfp, lbt, rbt]]):
+            raise AssertionError("LFP, LBT, RBT, RFP indexes must be provided")
+        elif not (0 < lfp < lbt < rbt < rfp < len(zs)):
+            raise AssertionError(
+                "Invalid LFP, LBT, RBT, RFP index values: maybe out of bounds or incorrect order?"
+            )
+
+        n_rgh = self.n_rgh
+        if isinstance(n_rgh, float):
+            self.n_rgh = np.full(len(self.zs) - 1, n_rgh)
+
+        elif len(n_rgh) - 1 != len(zs):
+            raise AssertionError(
+                "Invalid 'mannings_roughness', should be a float or array of length numpoints-1 "
+            )
+
+    def shift_elevations(self, k):
+        """
+        Add k to elevation cross section
+        """
+        self.zs = self.zs + k
+
+    @classmethod
+    def define_trapz_cross_section_width_slope_elevation(cls, **kwargs):
+        """
+        A trapezoidal cross-section has 8 points:
+           3 points for the left flood plain (a bank + a flat part)
+        +  3 points for the right flood plain (a flat part + bank)
+        +  2 points defining the bottom of the main channel
+
+        Key-Value arguments:
+        * 'bottom_elevation'          : elevation of bottom of channel
+        * 'flood_plain_width'         : width of the flood plain on each side
+        * 'main_channel_bottom_width' : width of the bottom of the main channel
+        * 'main_channel_depth'        : depth of main channel measured from bottom to elevation of flood plain
+        * 'main_channel_bank_slope'   : slope of main channel banks
+        * 'flood_plain_bank_slope'    : slope of flood plain banks
+        * 'flood_plain_bank_height    : height of flood plain bank
+        """
+        if "bottom_elevation" in kwargs:
+            bottom_elevation = kwargs["bottom_elevation"]
+        else:
+            bottom_elevation = 0
+
+        if "flood_plain_width" in kwargs:
+            flood_plain_width = kwargs["flood_plain_width"]
+            if flood_plain_width <= 0:
+                raise AssertionError("'flood_plain_width' should be a positive number")
+        else:
+            flood_plain_width = 22
+
+        if "main_channel_bottom_width" in kwargs:
+            main_channel_bottom_width = kwargs["main_channel_bottom_width"]
+            if main_channel_bottom_width <= 0:
+                raise AssertionError(
+                    "'main_channel_bottom_width' should be a positive number"
+                )
+        else:
+            main_channel_bottom_width = 20
+
+        if "main_channel_depth" in kwargs:
+            main_channel_depth = kwargs["main_channel_depth"]
+            if main_channel_depth <= 0:
+                raise AssertionError("'main_channel_depth' should be a positive number")
+        else:
+            main_channel_depth = 20
+
+        if "main_channel_bank_slope" in kwargs:
+            main_channel_bank_slope = kwargs["main_channel_bank_slope"]
+            if main_channel_bank_slope <= 0:
+                raise AssertionError(
+                    "'main_channel_bank_slope' should be a positive number"
+                )
+        else:
+            main_channel_bank_slope = 2 / 3
+
+        if "flood_plain_bank_slope" in kwargs:
+            flood_plain_bank_slope = kwargs["flood_plain_bank_slope"]
+            if flood_plain_bank_slope <= 0:
+                raise AssertionError(
+                    "'flood_plain_bank_slope' should be a positive number"
+                )
+        else:
+            flood_plain_bank_slope = 0.5
+
+        if "flood_plain_bank_height" in kwargs:
+            flood_plain_bank_height = kwargs["flood_plain_bank_height"]
+            if flood_plain_bank_height <= 0:
+                raise AssertionError(
+                    "'flood_plain_bank_height' should be a positive number"
+                )
+        else:
+            flood_plain_bank_height = 4
+
+        zs = np.array(
+            [
+                main_channel_depth + flood_plain_bank_height,
+                main_channel_depth,
+                main_channel_depth,
+                0,
+                0,
+                main_channel_depth,
+                main_channel_depth,
+                main_channel_depth + flood_plain_bank_height,
+            ]
+        )
+        zs = zs + bottom_elevation
+
+        half_chanel_w = np.array(
+            [
+                main_channel_bottom_width / 2,
+                main_channel_depth / main_channel_bank_slope,
+                flood_plain_width,
+                flood_plain_bank_height / flood_plain_bank_slope,
+            ]
+        )
+        half_chanel_w = half_chanel_w.cumsum()
+        ws = np.r_[-half_chanel_w[::-1], half_chanel_w]
+
+        lfp_idx, lbt_idx, rbt_idx, rfp_idx = 2, 3, 4, 5
+
+        return ws, zs, lfp_idx, lbt_idx, rbt_idx, rfp_idx
